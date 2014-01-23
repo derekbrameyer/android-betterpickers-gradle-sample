ba703d4f116e9e96ec6e02d5f40508fda0220298
Derek Brameyer <derek.brameyer@willowtreeapps.com>
2014-01-22 20:55:07 -0500

    Initial commit.

---
+++ MyApplication/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.myapplication;
+
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBarActivity;
+import android.text.TextUtils;
+import android.text.format.Time;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.doomonafireball.betterpickers.recurrencepicker.EventRecurrence;
+import com.doomonafireball.betterpickers.recurrencepicker.EventRecurrenceFormatter;
+import com.doomonafireball.betterpickers.recurrencepicker.RecurrencePickerDialog;
+import com.doomonafireball.betterpickers.timezonepicker.TimeZoneInfo;
+import com.doomonafireball.betterpickers.timezonepicker.TimeZonePickerDialog;
+
+public class MainActivity extends ActionBarActivity
+        implements RecurrencePickerDialog.OnRecurrenceSetListener, TimeZonePickerDialog.OnTimeZoneSetListener {
+
+    private TextView text;
+    private Button button;
+
+    private EventRecurrence mEventRecurrence = new EventRecurrence();
+
+    private static final String FRAG_TAG_RECUR_PICKER = "recurrencePickerDialogFragment";
+
+    private String mRrule;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text = (TextView) findViewById(R.id.text);
+        button = (Button) findViewById(R.id.button);
+
+        text.setText("--");
+        button.setText("Set Recurrence");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getSupportFragmentManager();
+                Bundle b = new Bundle();
+                Time t = new Time();
+                t.setToNow();
+                b.putLong(RecurrencePickerDialog.BUNDLE_START_TIME_MILLIS, t.toMillis(false));
+                b.putString(RecurrencePickerDialog.BUNDLE_TIME_ZONE, t.timezone);
+
+                // may be more efficient to serialize and pass in EventRecurrence
+                b.putString(RecurrencePickerDialog.BUNDLE_RRULE, mRrule);
+
+                RecurrencePickerDialog rpd = (RecurrencePickerDialog) fm.findFragmentByTag(
+                        FRAG_TAG_RECUR_PICKER);
+                if (rpd != null) {
+                    rpd.dismiss();
+                }
+                rpd = new RecurrencePickerDialog();
+                rpd.setArguments(b);
+                rpd.setOnRecurrenceSetListener(MainActivity.this);
+                rpd.show(fm, FRAG_TAG_RECUR_PICKER);
+            }
+        });
+    }
+
+    @Override
+    public void onRecurrenceSet(String rrule) {
+        mRrule = rrule;
+        if (mRrule != null) {
+            mEventRecurrence.parse(mRrule);
+        }
+        populateRepeats();
+    }
+
+    @Override
+    public void onTimeZoneSet(TimeZoneInfo tzi) {
+        text.setText(tzi.mDisplayName);
+    }
+
+    private void populateRepeats() {
+        Resources r = getResources();
+        String repeatString = "";
+        boolean enabled;
+        if (!TextUtils.isEmpty(mRrule)) {
+            repeatString = EventRecurrenceFormatter.getRepeatString(this, r, mEventRecurrence, true);
+        }
+
+        text.setText(mRrule + "\n" + repeatString);
+    }
+
+    @Override
+    public FragmentManager getSupportFragmentManager() {
+        return super.getSupportFragmentManager();
+    }
+}
